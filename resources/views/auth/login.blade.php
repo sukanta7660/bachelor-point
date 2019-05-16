61258f6d5101bbd4dc8dddb3803afc3e0e09dabb
Sukanta Purkayastha <30552975+Knightrider7660@users.noreply.github.com>
2019-05-16 10:51:30 +0600

    initial platform ready

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card card-login mx-auto mt-5">
+        <div class="card-header">Login</div>
+        <div class="card-body">
+            <form action="{{ route('login') }}" method="post" enctype="multipart/form-data">
+                @csrf
+                @error('email')
+                <span class="text-danger" role="alert">
+                    <strong>{{ $message }}</strong>
+                 </span>
+                @enderror
+                <div class="form-group">
+                    <div class="form-label-group">
+                        <input type="email" id="inputEmail" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        <label for="inputEmail">Email address</label>
+                    </div>
+                </div>
+                @error('password')
+                <span class="text-danger" role="alert">
+                    <strong>{{ $message }}</strong>
+                 </span>
+                @enderror
+                <div class="form-group">
+                    <div class="form-label-group">
+                        <input type="password" id="inputPassword" class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password" required autocomplete="current-password">
+                        <label for="inputPassword">Password</label>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" value="remember-me" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                            Remember Password
+                        </label>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">Login</button>
+            </form>
+            <div class="text-center">
+                <a class="d-block small mt-3" href="{{ route('register') }}">Register an Account</a>
+                @if (Route::has('password.request'))
+                <a class="d-block small" href="{{ route('password.request') }}">Forgot Password?</a>
+                @endif
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Bachelor-Point</title>
+    <!-- Custom fonts for this template-->
+    <link href="{{asset('public/assets/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <!-- Custom styles for this template-->
+    <link href="{{asset('public/assets/css/sb-admin.css')}}" rel="stylesheet">
+
+</head>
+
+<body class="bg-dark">
+
+<div class="container">
+    @yield('content')
+</div>
+
+<!-- Bootstrap core JavaScript-->
+<script src="{{asset('public/assets/vendor/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('public/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- Core plugin JavaScript-->
+<script src="{{asset('public/assets/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+</body>
+
+</html>
